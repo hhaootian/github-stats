a274bcb952de641bf5019a2841ca30fd57055364
Hao Tian <haot@mail.smu.edu>
2022-03-07 14:28:32 -0600

    supress output info

--- clone_repo.py
+++ clone_repo.py
@@ -11,11 +11,12 @@ def clone(username, token, repos):
         repo_name = repo.split("/")[-1]
         file_names.append(repo_name + ".txt")
 
-        repo_url = f"https://{username}:{token}@github.com/" \
-            + f"{username}/{repo_name}"
-        os.system("git clone " + repo_url)
+        repo_url = "/".join(repo.split("/")[-2:])
+        repo_url = f"https://{username}:{token}@github.com/" + repo_url
+
+        os.system(f"git clone {repo_url} -q")
 
         # run cloc
-        os.system(f"cloc {repo_name} --out {repo_name}.txt")
+        os.system(f"cloc {repo_name} --out {repo_name}.txt --quiet")
 
     os.system("cloc --sum-reports %s --out=everything" % " ".join(file_names))
